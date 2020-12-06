824e3f026c809ac8d33641d7fa7fe4805ad404a0
Chris <chrispyc@gmail.com>
2020-12-06 16:26:12 -0500

    Ok I guess redux is remembered

--- src/App.js
+++ src/App.js
@@ -1,7 +1,42 @@
 import React from "react";
+import { connect } from "react-redux";
+import { withRouter } from "react-router-dom";
 
-function App() {
-  return "nyes";
+function App(props) {
+  console.log(props);
+  return (
+    <div
+      onClick={(e) => {
+        props.addName(e, "random LUL");
+      }}
+    >
+      OK LETS TEST STUFF
+      {props.names.length > 0 ? <List names={props.names} /> : ""}
+    </div>
+  );
 }
 
-export default App;
+const List = ({ names }) => {
+  let Mapped = names.map((name) => {
+    return name;
+  });
+  return <div>{Mapped}</div>;
+};
+
+const mapStateToProps = ({ names, items }, ownProps) => {
+  return {
+    everyProp: ownProps,
+    names,
+    items,
+  };
+};
+const mapDispatchToProps = (dispatch) => {
+  return {
+    addName: (e, newName) => {
+      console.log(newName);
+      dispatch({ type: "ADD_NAME", payload: newName });
+    },
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(withRouter(App));
--- src/index.js
+++ src/index.js
@@ -1,10 +1,34 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import { BrowserRouter as Router } from "react-router-dom";
+const initialState = {
+  names: [],
+  items: [],
+};
+
+const reducer = (store = initialState, action) => {
+  if (action.type === "ADD_NAME") {
+    return {
+      ...store,
+      names: [...store.names, action.payload],
+    };
+  }
+
+  return store;
+};
+
+const store = createStore(reducer);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <Router>
+        <App />
+      </Router>
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
